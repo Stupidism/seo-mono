04989c018c01340f2d2425221579c65fd4074530
stupidism <sunfeng32@qq.com>
2022-07-01 13:49:36 +0800

    --wip-- [skip ci]

--- apps/seo-payload/src/collections/TestDrafts.ts
+++ apps/seo-payload/src/collections/TestDrafts.ts
@@ -6,6 +6,29 @@ export const TestDrafts: CollectionConfig = {
     useAsTitle: 'name',
   },
   versions: {
+    drafts: true,
+  },
+  access: {
+    read: ({ req: { user } }) => {
+      // users who are authenticated will see all posts
+      if (user) {
+        return true;
+      }
+
+      // query publishDate to control when posts are visible to guests
+      return {
+        and: [
+          {
+            publishDate: {
+              less_than: new Date().toJSON(),
+            },
+            _status: {
+              equals: 'published',
+            },
+          },
+        ],
+      };
+    },
   },
   // auth enabled collections get email and other fields for secure authentication in addition to the fields you add
   fields: [
@@ -14,6 +37,15 @@ export const TestDrafts: CollectionConfig = {
       type: 'text',
       required: true,
       unique: true,
+    },
+    {
+      name: 'publishDate',
+      type: 'date',
+      admin: {
+        position: 'sidebar',
+        description: 'Posts will not be public until this date',
+      },
+      defaultValue: () => new Date(),
     },
   ],
 };
